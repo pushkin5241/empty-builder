45ede7d4dda322e7af5974d856c34251e431352c
PUSH-KIN <pushkin5241@users.noreply.github.com>
2021-03-20 00:24:25 +0200

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,20 +1,20 @@
 const {mix} = require('laravel-mix');
 
-mix.setPublicPath('dist');
+mix.disableNotifications();
 
-/*mix.autoload({
-    jquery: ['$', 'window.jQuery', 'jQuery'],
-});*/
+mix.setPublicPath('dist');
 
 mix.options({
     processCssUrls: false,
 });
 
-mix.disableNotifications();
+mix.autoload({
+    jquery: ['$', 'window.jQuery', 'jQuery'],
+});
 
 mix.copy('src', 'dist', false);
 mix.copy('resources/images', 'dist/images', false);
 mix.copy('resources/fonts', 'dist/fonts', false);
 
-mix.sass('resources/styles/app.scss', 'dist/css')
-    .js('resources/scripts/app.js', 'dist/scripts');
+mix.sass('resources/styles/app.scss', 'dist/css');
+mix.js('resources/scripts/app.js', 'dist/scripts').extract();
